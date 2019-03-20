8c4392f857d68a6314d1fc2f762bb1aee9e691f7
Marvin Maciejewski <marvin.maciejewski@stud.hs-ruhrwest.de>
2019-03-20 15:21:20 +0100

    feat: add CORS to express

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const Joi = require('joi');
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PomodoroTimer = require('./Pomodoro/PomodoroTimer');
 const Timer = new PomodoroTimer(0, 0);
 
 app.use(express.json());
+app.use(cors());
 
 // Reads the .env-file
 const dotenv = require('dotenv');
